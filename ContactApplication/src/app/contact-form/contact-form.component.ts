76481f91b76b65c4adf0feffc24ee6b139b4576d
JosephRArias <31783008+JosephRArias@users.noreply.github.com>
2020-10-08 16:09:05 -0400

    Small fixes

--- ContactApplication/src/app/contact-form/contact-form.component.ts
+++ ContactApplication/src/app/contact-form/contact-form.component.ts
@@ -16,53 +16,68 @@ export class ContactFormComponent implements OnInit {
   index: number;
   Contact = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    email: new FormControl('',[Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     phoneNumber: new FormArray([new FormControl('', [Validators.required, Validators.pattern('[0-9]{10}'), Validators.minLength(10),
     Validators.maxLength(10)])]),
     address: new FormControl('')
-      });
+  });
   constructor(private dataService: DataService, private router: Router, private route: ActivatedRoute) {
     this.formArray = this.Contact.get('phoneNumber') as FormArray;
   }
 
   ngOnInit() {
-    this.index = this.route.snapshot.params['id'];
-    if(this.index){
+    this.index = this.route.snapshot.params.id;
+    if (this.index) {
       this.contact = this.dataService.findContact(this.index);
       this.Contact.patchValue({
         name: this.contact.name,
         email: this.contact.email,
-        phoneNumber: this.contact.phoneNumber,
         address: this.contact.address
       });
+      this.formArray.removeAt(0);
+      this.AddPhoneNumber();
+      /*
+      for(const phone in this.contact.phoneNumber){
+        console.log(this.contact.phoneNumber);
+        this.formArray.push(new FormControl(this.contact.phoneNumber[phone]));
+      }*/
     }
   }
-  onSubmit(){
+  onSubmit() {
     let data = this.Contact.value;
-    if(this.index){
-      this.dataService.modifyContact(this.index,data);
+    if (this.index) {
+      this.dataService.modifyContact(this.index, data);
       swal.fire('Contact Updated', 'Changes applied', 'success');
     }
-    else{
+    else {
       this.dataService.addNewContact(data);
       swal.fire('Contact Added', 'Contact added successfully', 'success');
     }
     this.router.navigate(['/']);
   }
-  discardData(){
+  discardData() {
     swal.fire({
       title: `Are you sure you wanna discard changes?`,
       icon: `question`,
-      showCancelButton:true,
+      showCancelButton: true,
+      cancelButtonColor: '#d33',
       showConfirmButton: true
-    }).then(res=>{
-      if(res.value){
+    }).then(res => {
+      if (res.value) {
         this.router.navigate(['/']);
       }
     });
   }
-  AddPhoneNumber(){
-    this.formArray.push(new FormControl('', [Validators.required, Validators.pattern('[0-9]{10}'),
-    Validators.minLength(10), Validators.maxLength(10)]));
+  AddPhoneNumber() {
+    if (this.index) {
+      this.contact.phoneNumber.forEach(x => {
+        this.formArray.push(new FormControl(x, [Validators.required, Validators.pattern('[0-9]{10}'),
+        Validators.minLength(10), Validators.maxLength(10)]));
+      });
+    }
+    else {
+      this.formArray.push(new FormControl('', [Validators.required, Validators.pattern('[0-9]{10}'),
+      Validators.minLength(10), Validators.maxLength(10)]));
+    }
   }
 }
